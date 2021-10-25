c81770091f0aca203ef7285be413bd2aa9db1194
Laura Court <laura.mcourt@gmail.com>
2021-10-24 20:15:30 -0400

    Add all merchants index

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::MerchantsController < ApplicationController
+
   def index
-    merchants = Merchant.all
+    per_page = params.fetch(:per_page, 20).to_i
+    if params[:page].to_i > 0
+      page = params.fetch(:page, 1).to_i - 1
+    else
+      page = 0
+    end
+    merchants = Merchant.offset(per_page * page).limit(per_page)
     render json: MerchantSerializer.new(merchants)
   end
 end
