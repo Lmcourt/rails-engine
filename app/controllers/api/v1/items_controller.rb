26ee0efd58c26c20fa8c540ab2566b1b055da6e8
Laura Court <laura.mcourt@gmail.com>
2021-10-25 18:45:45 -0400

    Add index, show, and create actions

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::ItemsController < ApplicationController
+
+  def index
+    render json: ItemSerializer.new(all_items)
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: ItemSerializer.new(item)
+  end
+
+  def create
+    item = Item.create!(item_params)
+    render json: ItemSerializer.new(item), status: 201
+  end
+
+  private
+
+  def all_items
+    if params[:merchant_id]
+      Merchant.find(params[:merchant_id]).items
+    else
+      Item.offset(per_page * page).limit(per_page)
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
+end
