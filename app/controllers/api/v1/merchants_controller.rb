75daafd40acaf686a5d27b975271011696347f69
Laura Court <laura.mcourt@gmail.com>
2021-10-24 20:15:52 -0400

    Add find merchant by id

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -10,4 +10,9 @@ class Api::V1::MerchantsController < ApplicationController
     merchants = Merchant.offset(per_page * page).limit(per_page)
     render json: MerchantSerializer.new(merchants)
   end
+
+  def show
+    merchant = Merchant.find(params[:id])
+    render json: MerchantSerializer.new(merchant)
+  end
 end
