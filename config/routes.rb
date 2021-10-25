06f17da9eae53dc90bf0b63d76dc045b302d8973
Laura Court <laura.mcourt@gmail.com>
2021-10-25 18:46:48 -0400

    Add items resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants do
-        resources :items
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: :index
       end
+      resources :items
     end
   end
 end
