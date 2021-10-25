935ff1031b781ea2eaa3c915a0e2c6e13c0cd252
Laura Court <laura.mcourt@gmail.com>
2021-10-25 18:46:30 -0400

    Add per_page and page methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 class ApplicationController < ActionController::API
   include ExceptionHandler
   include Response
+
+  private
+
+  def per_page
+    page_size = params.fetch(:per_page, 20).to_i
+    if params[:per_page].to_i > 0
+      page_size
+    else
+      20
+    end
+  end
+
+  def page
+    if params[:page].to_i > 0
+      params.fetch(:page, 1).to_i - 1
+    else
+      0
+    end
+  end
 end
