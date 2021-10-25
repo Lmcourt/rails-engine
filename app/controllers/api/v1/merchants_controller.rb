16e72cb5318521edea5be39a29755aaa4e9876ef
Laura Court <laura.mcourt@gmail.com>
2021-10-25 18:46:18 -0400

    Remove per_page and page variables

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,12 +1,6 @@
 class Api::V1::MerchantsController < ApplicationController
 
   def index
-    per_page = params.fetch(:per_page, 20).to_i
-    if params[:page].to_i > 0
-      page = params.fetch(:page, 1).to_i - 1
-    else
-      page = 0
-    end
     merchants = Merchant.offset(per_page * page).limit(per_page)
     render json: MerchantSerializer.new(merchants)
   end
