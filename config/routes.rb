01758cfd5d7582d06b142f083dff411431270b61
Laura Court <laura.mcourt@gmail.com>
2021-10-26 18:28:01 -0400

    Add routes for find and find_all

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'merchants/find', to: 'merchants/search#find'
+      get 'items/find_all', to: 'items/search#find_all'
       resources :merchants, only: [:index, :show] do
         resources :items, only: :index
       end
