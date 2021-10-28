235948b13b81fa1f7db7800d1ff24cae7b64e5b2
Laura Court <laura.mcourt@gmail.com>
2021-10-28 19:10:33 -0400

    Add merchant most revenue and single merchant revenue methods

---
+++ app/controllers/api/v1/revenue/merchants_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::Revenue::MerchantsController < ApplicationController
+
+  def merchants_most_revenue
+    merchants = Merchant.ordered_total_revenue(params[:quantity])
+    render json: MerchantNameRevenueSerializer.new(merchants)
+
+    raise ActionController::BadRequest unless params[:quantity]
+  end
+
+  def merchant_revenue
+    validate_merchant
+    merchant = Merchant.merchant_total_revenue(params[:merchant_id])
+    render json: MerchantRevenueSerializer.new(merchant)
+  end
+
+  private
+  def validate_merchant
+    if params[:merchant_id]
+      merchant = Merchant.find_by(id: params[:merchant_id])
+      raise ActiveRecord::RecordNotFound unless merchant
+    end
+  end
+end
