5486b8c52c7d747f896b75ff5d836b7c5456f147
Laura Court <laura.mcourt@gmail.com>
2021-10-28 19:55:38 -0400

    Add merchant relationship

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,5 +3,5 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
-  has_many :merchants, through: :items
+  belongs_to :merchant
 end
