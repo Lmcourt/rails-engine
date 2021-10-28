7dd7e471ed9f7e907ad5c0d0f8d8b454e8eb34d9
Laura Court <laura.mcourt@gmail.com>
2021-10-28 19:10:08 -0400

    Add find method 400 response

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def find
-    search = Merchant.search_by_name(params[:name]).first
-    render json: MerchantSerializer.new(search)
+    merchant = Merchant.search_by_name(params[:name]).first
+    if merchant
+      render json: MerchantSerializer.new(merchant)
+    else
+      render json: { data: {
+        message: 'Merchant not found',
+        status: 400
+        } }
+    end
   end
 end
