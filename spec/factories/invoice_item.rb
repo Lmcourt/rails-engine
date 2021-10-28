b3467a72eec5edba27b073b936673b0788136a39
Laura Court <laura.mcourt@gmail.com>
2021-10-28 19:13:20 -0400

    Add factory

---
+++ spec/factories/invoice_item.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :invoice_item, class: InvoiceItem do
+    quantity { Faker::Number.number(digits: 4)}
+    unit_price { Faker::Number.number(digits: 2) }
+    created_at { "2019-02-27 10:52:09 UTC" }
+    updated_at { "2020-03-27 14:32:09 UTC" }
+  end
+end
