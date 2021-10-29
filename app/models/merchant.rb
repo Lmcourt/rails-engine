728b1caad3d6eb1a7dc3755326c7630e40023de9
Laura Court <laura.mcourt@gmail.com>
2021-10-28 20:19:10 -0400

    Adjust invoices relationship

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,12 +1,12 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoice_items, through: :items
-  has_many :invoices, through: :invoice_items
+  has_many :invoices
   has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
 
   def self.ordered_total_revenue(quantity)
-    joins(invoices: :transactions)
+    joins(invoices: [:transactions, :invoice_items])
     .where(invoices: {status: 'shipped'}, transactions: {result: 'success'})
     .select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue')
     .group(:id)
@@ -15,7 +15,7 @@ class Merchant < ApplicationRecord
 
   end
 
-  def self.merchant_total_revenue(merch)
+  def merchant_total_revenue
     joins(invoices: :transactions)
     .where('merchants.id = ? AND result = ?', merch, 'success')
     .select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue')
